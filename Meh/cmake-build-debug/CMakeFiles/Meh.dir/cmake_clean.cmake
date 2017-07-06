dad9a85664b2db5a8d6c7b4acd049528f9c2b9bd
Taimur <tai.him18@gmail.com>
2017-07-06 21:08:39 +0500

    ALL teh files

---
+++ Meh/cmake-build-debug/CMakeFiles/Meh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Meh.dir/main.cpp.o"
+  "Meh.pdb"
+  "Meh"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Meh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
